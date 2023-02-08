ee15c844ac465cb86234f58dd4b294859a599875
hernanml1988 <97610098+hernanml1988@users.noreply.github.com>
2023-02-08 16:27:30 -0400

    nav header con menu responsive, listo

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ d.addEventListener("click", (e) => {
   if (e.target.matches(".flecha_menu") || e.target.matches(".flecha_menu *")) {
     let nav = document.querySelector("nav");
     let menu = document.querySelectorAll("nav > div");
+    let contenedor = document.querySelectorAll("body > div");
+
     if (nav.classList == "nav_header") {
       nav.classList.remove("nav_header");
       nav.classList.add("nav_header_responsive");
@@ -15,16 +17,13 @@ d.addEventListener("click", (e) => {
       menu[1].classList.add("menu");
       menu[1].classList.remove("menu_responsive");
     }
-    console.log(nav);
-
-    // let menu = document.querySelectorAll("nav > div");
-    // if (menu[1].classList == "menu") {
-    //   menu[1].classList.remove("menu");
-    //   menu[1].classList.add("menu_responsive");
-    // } else {
-    //   menu[1].classList.add("menu");
-    //   menu[1].classList.remove("menu_responsive");
-    // }
+    console.log(contenedor[1]);
+    if (contenedor[1].classList == "container") {
+      contenedor[1].classList.remove("container");
+      contenedor[1].classList.add("container_responsive");
+    } else {
+      contenedor[1].classList.add("container");
+      contenedor[1].classList.remove("container_responsive");
+    }
   }
-  //   console.log(e);
 });
